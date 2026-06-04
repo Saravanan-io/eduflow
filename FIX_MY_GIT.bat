9a67d7fd9bf704cc68213046586b948adc38373d
J Saravanan <thisissaravanan84@gmail.com>
2026-06-04 15:26:51 +0530

    Remove FIX_MY_GIT.bat

--- FIX_MY_GIT.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Staging all uncommitted changes...
-git add .
-echo Committing changes...
-git commit -m "Save uncommitted changes before worktree move"
-echo.
-echo All set! Your workspace is now clean. You can retry your worktree move.
-pause
